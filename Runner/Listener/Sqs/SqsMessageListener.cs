eb7065c1b39fc356559240e6c328e8aabbcb1c83
Jake Laurie <jake.gordon.laurie@gmail.com>
2025-05-08 20:04:32 +1200

    Fix message deletion

--- Runner/Listener/Sqs/SqsMessageListener.cs
+++ Runner/Listener/Sqs/SqsMessageListener.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Lookout.Runner.Listener.Sqs;
 
-public record SqsProviderData(string ReceiptHandle, string MessageId);
+public record SqsProviderData(string ReceiptHandle, string MessageId, string QueueUrl);
 
 public class SqsMessageListener(IAmazonSQS sqsClient, ILogger<SqsMessageListener> logger) : IQueueListener<SqsProviderData>
 {
@@ -22,7 +22,7 @@ public class SqsMessageListener(IAmazonSQS sqsClient, ILogger<SqsMessageListener
             {
                 foreach (var msg in message.Messages)
                 {
-                    var parsedMessage = ParseMessage(msg);
+                    var parsedMessage = ParseMessage(msg, queue);
                     if (parsedMessage == null)
                     {
                         logger.LogError($"Failed to deserialize message body");
@@ -55,14 +55,23 @@ public class SqsMessageListener(IAmazonSQS sqsClient, ILogger<SqsMessageListener
     // Delete the message from the queue
     public async Task ConfirmReceipt(QueueMessage<SqsProviderData> message)
     {
-        await sqsClient.DeleteMessageAsync(new DeleteMessageRequest
+        logger.LogDebug("Confirming receipt of message: {MessageId}", message.ProviderData.MessageId);
+        var res = await sqsClient.DeleteMessageAsync(new DeleteMessageRequest
         {
-            QueueUrl = message.ImageDescription.Name,
-            ReceiptHandle = message.ImageDescription.Tag
+            QueueUrl = message.ProviderData.QueueUrl,
+            ReceiptHandle = message.ProviderData.ReceiptHandle 
         });
+
+        if (res.HttpStatusCode == System.Net.HttpStatusCode.OK)
+        {
+            logger.LogDebug("Confirmed receipt of message: {MessageId}", message.ProviderData.MessageId);
+        } else
+        {
+            logger.LogError("Failed to confirm receipt of message: {MessageId}, with code {Code}", message.ProviderData.MessageId, res.HttpStatusCode);
+        }
     }
 
-    private QueueMessage<SqsProviderData>? ParseMessage(Message? message)
+    private QueueMessage<SqsProviderData>? ParseMessage(Message? message, string queueUrl)
     {
         if (message == null)
         {
@@ -78,7 +87,7 @@ public class SqsMessageListener(IAmazonSQS sqsClient, ILogger<SqsMessageListener
             }
 
             var imageDescription = new ImageDescription(body.ImageName, body.ImageTag);
-            var providerData = new SqsProviderData(message.ReceiptHandle, message.MessageId);
+            var providerData = new SqsProviderData(message.ReceiptHandle, message.MessageId, queueUrl);
 
             TimeSpan? deployTime = null;
             if (body.DeployTimeSeconds != null && int.TryParse(body.DeployTimeSeconds, out var deployTimeSeconds))

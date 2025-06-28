c0267d35e94e18e7f01c1f1e87578ca910a935a2
Jake Laurie <5002212+xtenduke@users.noreply.github.com>
2025-06-28 16:38:10 +1200

    feat(sqs): Implement long polling (#9)
    * Add PollTimeSeconds to config for sqs long polling
    
    * Implement long polling and trycatch for the listen loop with backoff
    
    * Log poll value on validation fail

--- Runner/Environment/Environment.cs
+++ Runner/Environment/Environment.cs
@@ -10,10 +10,16 @@ public record Config
     public string? RegistryPassword { get; init; }
     public string? HostId { get; init; }
 
+    public int PollTimeSeconds { get; init; } = 1;
+
     public void Validate()
     {
 
         if (string.IsNullOrWhiteSpace(SqsQueueUrl))
             throw new ArgumentException("SqsQueueUrl must not be empty.");
+
+        if (PollTimeSeconds < 1 || PollTimeSeconds > 20) {
+            throw new ArgumentException($"Invalid poll time, must be between 1 and 20 inclusive. Value: {PollTimeSeconds}");
+        }
     }
 }
